dc6d00e86aed453974b93e9cdc958b9112357c01
Kapkaev Andrey <kapkaew@yandex.ru>
2022-05-06 21:41:22 +0300

    Update forms information

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -11,17 +11,20 @@ from .models import URL_map
 def get_origin_url(short_id):
     url_map = URL_map.query.filter_by(short=short_id).first()
     if url_map is None:
-        raise InvalidAPIUsageError('Указанный id не найден', 404)
-    return jsonify({'url': url_map.original}), 200
+        raise InvalidAPIUsageError(
+            'Указанный id не найден', HTTPStatus.NOT_FOUND
+        )
+    return jsonify({'url': url_map.original}), HTTPStatus.OK
 
 
-@app.route('/api/id/', methods=['POST'])
+@app.route('/api/id/', methods=['POST'])  #TODO замена аналаиз на броски исключений из валидирующего метода
 def add_short_link():
     data = request.get_json()
     if data is None:
         raise InvalidAPIUsageError('Отсутствует тело запроса')
     if 'url' not in data:
         raise InvalidAPIUsageError('"url" является обязательным полем!')
+    #####################################################################
     if 'custom_id' in data and data['custom_id']:
         is_valid, message = URL_map.is_valid_short_id(data['custom_id'])
         if not is_valid:
@@ -30,6 +33,7 @@ def add_short_link():
             )
     else:
         data['custom_id'] = URL_map.get_unique_short_id()
+    ######################################################################
     url_map = URL_map()
     url_map.from_dict(data)
     db.session.add(url_map)
--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import URL, DataRequired, Length, Optional, Regexp
+
 from settings import LINK_MATCHING_PATTERN, MAX_LENGTH_LINK, MAX_LENGTH_URL
 
 
@@ -9,14 +10,14 @@ class UrlMapForm(FlaskForm):
         'Ссылка для сокращения',
         validators=[
             DataRequired(message='Обязательное поле'),
-            Length(2, MAX_LENGTH_URL),
+            Length(1, MAX_LENGTH_URL, 'Недопустимая длинна URL'),
             URL(message='Не корректный адрес'),
         ]
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
         validators=[
-            Length(1, MAX_LENGTH_LINK),
+            Length(1, MAX_LENGTH_LINK, 'Недопустимая длинна URL'),
             Regexp(
                 LINK_MATCHING_PATTERN,
                 message='Ссылка должна содержать только цифры и'
